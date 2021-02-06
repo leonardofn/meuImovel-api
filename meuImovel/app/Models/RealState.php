d0475526bf6f13414aee80d05789d23fa991623f
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2021-02-06 14:55:01 -0300

    Iniciando Endpoint

---
+++ meuImovel/app/Models/RealState.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RealState extends Model
+{
+    protected $table = 'real_state';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- meuImovel/routes/api.php
+++ meuImovel/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function(){
+
+    Route::prefix('real-states')->name('real_states.')->group(function(){
+
+        Route::get('/', 'App\Http\Controllers\Api\RealStateController@index')->name('index');
+
+    });
+});
