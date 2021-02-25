a4ac7460ca5ac1d5b0600ebf448cc3499639d520
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2021-02-25 09:39:03 -0300

    Migration de Endereços

--- meuImovel/database/migrations/2021_02_23_211809_create_table_adresses.php
+++ meuImovel/database/migrations/2021_02_23_211809_create_table_adresses.php
@@ -14,8 +14,20 @@ class CreateTableAdresses extends Migration
     public function up()
     {
         Schema::create('adresses', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('states_id');
+            $table->unsignedBigInteger('city_id');
+            
+            $table->string('address');
+            $table->string('number');
+            $table->string('neighborhood');
+            $table->string('complement')->nullable();
+            $table->integer('zip_code');
+
             $table->timestamps();
+
+            $table->foreign('states_id')->references('id')->on('states');
+            $table->foreign('city_id')->references('id')->on('cities');
         });
     }
 
